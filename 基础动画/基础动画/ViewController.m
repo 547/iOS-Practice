e99ea116ea864ca3cf8cc0ee17f5b1e689699bb6
547 <1093031562@qq.com>
2016-03-15 20:42:44 +0800

    图层基础动画

--- 基础动画/基础动画/ViewController.m
+++ 基础动画/基础动画/ViewController.m
@@ -131,20 +131,20 @@
      push     //新视图把旧视图推出去
      moveIn   //新视图移到旧视图上面
      reveal   //将旧视图移开,显示下面的新视图
-     cube     //立方体翻滚效果
+     cube     //立方体翻滚效果(私有)
      oglFlip  //上下左右翻转效果
      suckEffect   //收缩效果，如一块布被抽走(不支持过渡方向)
      rippleEffect //滴水效果(不支持过渡方向)
      pageCurl     //向上翻页效果
      pageUnCurl   //向下翻页效果
-     cameraIrisHollowOpen  //相机镜头打开效果(不支持过渡方向)
-     cameraIrisHollowClose //相机镜头关上效果(不支持过渡方向)
+     cameraIrisHollowOpen  //相机镜头打开效果(不支持过渡方向)(私有)
+     cameraIrisHollowClose //相机镜头关上效果(不支持过渡方向)(私有)
      */
 
     tr.duration=2.0;
     tr.type=@"rippleEffect";
 
-    [self.mainImage.layer addAnimation:tr forKey:kCATransitionFade];
+    [self.mainImage.layer addAnimation:tr forKey:nil];
     self.mainImage.image=[UIImage imageNamed:[NSString stringWithFormat:@"%d.png",tagB]];
 }
 
